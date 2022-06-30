cf09f3866fa17fc29210fc6f7e25680dc37546c6
Forester04 <erickforester04@gmail.com>
2022-06-30 16:45:38 +0000

    Using puts to print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#inclde <stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzel");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
