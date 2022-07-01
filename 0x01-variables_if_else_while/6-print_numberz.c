87d07d5845aec82bf48c41f2220e27f9ad3b86be
Forester04 <erickforester04@gmail.com>
2022-07-01 23:32:47 +0000

    Single digit numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
