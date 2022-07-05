21a85d403a4023a42e4b5de04c3950b46d7f94ce
Forester04 <erickforester04@gmail.com>
2022-07-05 19:34:39 +0000

    Task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * main - prints the string "_putchar" from a character array.
+ *
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	char c[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
